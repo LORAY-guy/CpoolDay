e6413a681de58add97ebdc95eba2caa207b26ce0
LORAY <105822831+LORAY-guy@users.noreply.github.com>
2024-04-17 09:54:16 +0000

    commit

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -2,6 +2,7 @@ void print_char(char c);
 
 void my_print_ascii(void)
 {
+
     for (int i = 32; i <= 127; i++)
     {
         print_char(i);
--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -2,9 +2,12 @@ void print_char(char c);
 
 void my_print_n_ascii(int n)
 {
-    for (int i = 32; i <= (32+n); i++)
+    if (n > 0)
     {
-        print_char(i);
+        for (int i = 32; i <= (32+n); i++)
+        {
+            print_char(i);
+        }
     }
 }
 
