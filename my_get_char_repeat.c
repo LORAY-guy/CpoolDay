60b467dd75a085d09e6e20fdb76969b8374e7cc0
LORAY <105822831+LORAY-guy@users.noreply.github.com>
2024-04-17 11:28:35 +0000

    commit

--- my_get_char_repeat.c
+++ my_get_char_repeat.c
@@ -3,7 +3,7 @@ int my_get_char_repeat(char to_find, const char *to_search)
     int num = 0;
     for (int i = 0; to_search[i] != 0; i++)
     {
-        if (i == to_find)
+        if (to_search[i] == to_find)
         {
             num++;
         }
