187dd094a2d8b8d59c234fce959e489cf93dbc1f
LORAY <105822831+LORAY-guy@users.noreply.github.com>
2024-04-17 10:09:16 +0000

    commit

--- my_print_revert.c
+++ my_print_revert.c
@@ -2,5 +2,14 @@ void print_char(char c);
 
 void my_print_revert(char *toRevert)
 {
-    
+    for (int i = sizeof(toRevert); i >= 0 ; i--)
+    {
+        print_char(toRevert[i]);
+    }
 }
+
+/*int main(void)
+{
+    my_print_revert("codingclub");
+    return 0;
+}*/
