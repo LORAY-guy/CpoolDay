da369b3e6b3fbb7c4275ee4ee7ba6feb289778d0
LORAY <105822831+LORAY-guy@users.noreply.github.com>
2024-04-17 11:31:46 +0000

    commit

--- my_get_char_repeat.c
+++ my_get_char_repeat.c
@@ -1,20 +1,25 @@
 int my_get_char_repeat(char to_find, const char *to_search)
 {
-    int num = 0;
-    for (int i = 0; to_search[i] != 0; i++)
+    if (to_search)
     {
-        if (to_search[i] == to_find)
+        int num = 0;
+        for (int i = 0; to_search[i] != 0; i++)
         {
-            num++;
+            if (to_search[i] == to_find)
+            {
+                num++;
+            }
         }
-    }
 
-    return num;
+        return num;
+    } else {
+        return 0;
+    }
 }
 
-/*#include <stdio.h>
+#include <stdio.h>
 
-int main(void)
+/*int main(void)
 {
     char str[] = "bienvenue à epitech !";
     printf("%d\n", my_get_char_repeat('e', str));
