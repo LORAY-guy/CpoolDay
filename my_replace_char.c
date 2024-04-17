86e9abed5bc2e512bf5af449592db368a0b6d189
LORAY <105822831+LORAY-guy@users.noreply.github.com>
2024-04-17 10:20:02 +0000

    commit

---
+++ my_replace_char.c
@@ -0,0 +1,21 @@
+void print_char(char c);
+
+char *my_replace_char(char *origin, char toFind, char toReplace) 
+{
+    for (int i = 0; i <= sizeof(*origin); i++)
+    {
+        if (origin[i] == toFind)
+        {
+            origin[i] == toReplace;
+        }
+    }
+}
+
+#include <stdio.h>
+
+int main(int ac, char **av)
+{
+    char str[] = "bienvenue à epitech !";
+    printf("%s\n", my_replace_char(str, 'e', 'a'));
+    return 0;
+}
